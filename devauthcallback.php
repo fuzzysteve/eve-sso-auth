64c141a51739472fa97ed14856a0324475d28ce2
Steve Anderson / Steve Ronuken <steve@fuzzysteve.co.uk>
2022-02-14 12:11:20 +0000

    Update devauthcallback.php

--- devauthcallback.php
+++ devauthcallback.php
@@ -1,9 +1,9 @@
 <?php
-require_once('auth_functions.php');
-require_once('secret.php');
 session_start();
+require_once('auth_functions.php');
+require_once('secretreal.php');
 
-$useragent="Fuzzwork Auth agent.";
+$useragent="Fuzzwork Auth agent 1.0";
 
 // Make sure that the secret matches the one set before the redirect.
 if (isset($_SESSION['auth_state']) and isset($_GET['state']) and $_SESSION['auth_state']==$_GET['state']) {
@@ -12,8 +12,8 @@ if (isset($_SESSION['auth_state']) and isset($_GET['state']) and $_SESSION['auth
 
 
     //Do the initial check.
-    $url='https://login.eveonline.com/oauth/token';
-    $verify_url='https://login.eveonline.com/oauth/verify';
+    $url='https://login.eveonline.com/v2/oauth/token';
+#    $verify_url='https://login.eveonline.com/oauth/verify';
     $header='Authorization: Basic '.base64_encode($clientid.':'.$secret);
     $fields_string='';
     $fields=array(
@@ -45,31 +45,37 @@ if (isset($_SESSION['auth_state']) and isset($_GET['state']) and $_SESSION['auth
 
 // Get the Character details from SSO
 
-    $header='Authorization: Bearer '.$auth_token;
-    curl_setopt($ch, CURLOPT_URL, $verify_url);
-    curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array($header));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-    $result = curl_exec($ch);
-    if ($result===false) {
-        auth_error(curl_error($ch));
-    }
-    curl_close($ch);
-    $response=json_decode($result);
+#    $header='Authorization: Bearer '.$auth_token;
+#    curl_setopt($ch, CURLOPT_URL, $verify_url);
+#    curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
+#    curl_setopt($ch, CURLOPT_HTTPHEADER, array($header));
+#    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+#    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+#    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+#    $result = curl_exec($ch);
+#    if ($result===false) {
+#        auth_error(curl_error($ch));
+#    }
+#    curl_close($ch);
+#    $response=json_decode($result);
 
 
-    if (!isset($response->CharacterID)) {
-        auth_error('No character ID returned');
-    }
+
+#    if (!isset($characterid)) {
+#        auth_error('No character ID returned');
+#    }
+
+$jwtexplode=json_decode(base64_decode(str_replace('_', '/', str_replace('-','+',explode('.',$auth_token )[1]))));
+$charactername=$jwtexplode->name;
+$owner=$jwtexplode->owner;
+$characterid=explode(":",$jwtexplode->sub)[2];
 
 // Lookup the character details in the DB.
     require_once('db.inc.php');
     $sql="select corporationname,corporationticker,user.corporationid,
     alliancename,allianceticker,corporation.allianceid,characterid,characterownerhash,
     user.id
-    from user 
+    from user
     join corporation on user.corporationid=corporation.corporationid
     join alliance on corporation.allianceid=alliance.allianceid
     where
@@ -78,7 +84,7 @@ if (isset($_SESSION['auth_state']) and isset($_GET['state']) and $_SESSION['auth
     ";
 
     $stmt = $dbh->prepare($sql);
-    $stmt->execute(array(':characterid'=>$response->CharacterID,':characterhash'=>$response->CharacterOwnerHash));
+    $stmt->execute(array(':characterid'=>$characterid,':characterhash'=>$owner));
 
     while ($row = $stmt->fetchObject()) {
         $userdetails=$row;
@@ -91,7 +97,7 @@ if (isset($_SESSION['auth_state']) and isset($_GET['state']) and $_SESSION['auth
         // No database entry for the user. lookup time.
         error_log('Creating user details');
         $ch = curl_init();
-        $lookup_url="https://esi.evetech.net/latest/characters/".$response->CharacterID."/";
+        $lookup_url="https://esi.evetech.net/latest/characters/".$characterid."/";
         curl_setopt($ch, CURLOPT_URL, $lookup_url);
         curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -128,7 +134,7 @@ if (isset($_SESSION['auth_state']) and isset($_GET['state']) and $_SESSION['auth
                 $alliance_data=json_decode($result);
                 $allianceticker=$alliance_data->ticker;
                 $allianceName=$alliance_data->name;
-                $alliance_insert_sql="insert into alliance (allianceid,alliancename,allianceticker) 
+                $alliance_insert_sql="insert into alliance (allianceid,alliancename,allianceticker)
                     values (:allianceid,:alliancename,:allianceticker)";
                 $stmt = $dbh->prepare($alliance_insert_sql);
                 $stmt->execute(
@@ -190,9 +196,9 @@ if (isset($_SESSION['auth_state']) and isset($_GET['state']) and $_SESSION['auth
         $stmt = $dbh->prepare($user_creation_sql);
         $stmt->execute(
             array(
-            ':characterid'=>$response->CharacterID,
-            ':characterownerhash'=>$response->CharacterOwnerHash,
-            ':character_name'=>$response->CharacterName,
+            ':characterid'=>$characterid,
+            ':characterownerhash'=>$owner,
+            ':character_name'=>$charactername,
             ':corporationid'=>$corporationID
             )
         );
@@ -202,14 +208,15 @@ if (isset($_SESSION['auth_state']) and isset($_GET['state']) and $_SESSION['auth
         error_log("user added to db");
     }
 
-    $_SESSION['auth_characterid']=$response->CharacterID;
+    $_SESSION['auth_characterid']=$characterid;
     $_SESSION['auth_id']=$userid;
-    $_SESSION['auth_charactername']=$response->CharacterName;
+    $_SESSION['auth_charactername']=$charactername;
     $_SESSION['auth_userdetails']=json_encode($userdetails);
-    $_SESSION['auth_characterhash']=$response->CharacterOwnerHash;
+    $_SESSION['auth_characterhash']=$owner;
     session_write_close();
+    setcookie('fuzzworkauth', $_SESSION['auth_characterid'], 0, '/', 'www.fuzzwork.co.uk', true, true);
     header('Location:'. $_SESSION['auth_redirect']);
-    
+
     exit;
 
 } else {
